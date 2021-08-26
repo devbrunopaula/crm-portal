9c289b4c2a7705cb4af87aa8c4a607c26688e6d3
Bruno Paula <devbrunopaula@gmail.com>
2021-08-26 16:19:52 -0400

    change envs

--- config/database.ts
+++ config/database.ts
@@ -35,13 +35,13 @@ const databaseConfig: DatabaseConfig = {
     */
     pg: {
       client: 'pg',
-      // connection: {
-      //   host: Env.get('PG_HOST'),
-      //   port: Env.get('PG_PORT'),
-      //   user: Env.get('PG_USER'),
-      //   password: Env.get('PG_PASSWORD', ''),
-      //   database: Env.get('PG_DB_NAME'),
-      // },
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
       migrations: {
         naturalSort: true,
       },
